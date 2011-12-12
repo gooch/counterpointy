a863ff3998ae3daa7bb90e3752815d6534586c50
Ben Williamson <benw@pobox.com>
2011-12-12 05:10:04 +0000

    expandable add supporting/opposing

--- public/counterpointy.js
+++ public/counterpointy.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
         document.location = '/point/' + $(this).data('pointHash');
     });
 
+    $('.expandable').click(function () {
+        $(this).hide().next().show().children('textarea').focus();
+    });
+
+    $('.expandable').next().hide();
+
 });
