44d6f32ed7c0f0787ba1969bfb882935598359b6
Ben Williamson <benw@pobox.com>
2011-12-21 01:05:35 +0000

    ms.js fixes

--- ms.js
+++ ms.js
@@ -2,22 +2,24 @@
 
 # ms.js - https://gist.github.com/1503944
 
-No more painful `setTimeout(60 * 4 * 3 * 2 * 1 * Infinity * NaN * '☃')`.
+No more painful `setTimeout(fn, 60 * 4 * 3 * 2 * 1 * Infinity * NaN * '☃')`.
 
     ms('2d')      // 172800000
     ms('1.5h')    // 5400000
     ms('1h')      // 3600000
     ms('1m')      // 60000
-    ms('5ms')     // 5000
-    ms('100')     // '100'
+    ms('5s')      // 5000
+    ms('500ms')   // 500
+    ms('100')     // 100
     ms(100)       // 100
 
 **/
 
 (function () {
   var _ = {}
-  _.ms = 1000;
-  _.m = _.ms * 60;
+  _.ms = 1;
+  _.s = 1000;
+  _.m = _.s * 60;
   _.h = _.m * 60;
   _.d = _.h * 24;
 
